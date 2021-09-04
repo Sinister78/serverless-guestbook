5a90c49cbad400d3bcd8cc60ef07b3192ffa5282
Sinister78 <31302264+Sinister78@users.noreply.github.com>
2021-09-03 23:05:24 -0600

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://499f2840.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://b91c8590.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
